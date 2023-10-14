97822160ff80eeb2b54c7bca18f715cf12ef8803
robertIsaac <dev.robert.isaac@gmail.com>
2023-10-15 00:59:35 +0300

    try to use esm

--- apps/nx-nest-esm/webpack.config.js
+++ apps/nx-nest-esm/webpack.config.js
@@ -1,8 +1,10 @@
-const { composePlugins, withNx } = require('@nx/webpack');
+import nxWebpack from '@nx/webpack';
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => {
+const exports = nxWebpack.composePlugins(nxWebpack.withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;
 });
+
+export default exports;
